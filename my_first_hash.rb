8396402189c88785882cb63490ec07790f562b8e
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-24 14:53:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,10 +12,10 @@ def retrieval
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
-                } the_manifest["oil_paintings"] 
+                }  
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -25,7 +25,7 @@ def adding
                   "oil paintings" => 3
                 }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest[:muskets] = 2 
 
 
   # add 4 gun powder to the shipping_manifest hash below
